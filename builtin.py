6f8a2243c3bdef3f3c7ced99884899d8a496f97d
salispoint45 <obisco45@yahoo.com>
2022-02-19 21:28:55 -0500

    Three Builtin

---
+++ builtin.py
@@ -0,0 +1,6 @@
+#Three builtin and what they do
+Tuple is built-in immutable sequence. Python tuples are a data structure that store an ordered sequence of values. Tuples are immutable. This means you cannot change the values in a tuple. ... They let you store an ordered sequence of items. For example, you may use a tuple to store a list of employee names
+
+Object: Everything is in Python treated as an object, including variable, function, list, tuple, dictionary, set, etc. Every object belongs to its class. For example - An integer variable belongs to integer class. An object is a real-life entity.
+
+range: The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
