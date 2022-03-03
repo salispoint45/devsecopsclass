9630cdff1bf7301c71fcf64c5dd8232faa5e7e11
salispoint45 <obisco45@yahoo.com>
2022-03-02 23:39:46 -0500

    common python

---
+++ question_of_the_day_feb22.py
@@ -0,0 +1,36 @@
+#what is the maximum length of a python idntifier?
+Answer = 79
+
+#which of the following is an invalid statement
+num = 1000000
+x y z = 1 2 3
+x,y,z = 1, 2, 3
+x_y_z = 10000000
+
+Answer = x y z = 1 2 3
+
+#Which of the following are valid Python variable names:
+ver1.3
+return
+home_address
+route66
+Age
+4square
+
+answer = ver1.3, home_address, route66, Age
+#All keywords in Python are in _________ *
+Lower case
+Upper case
+None of the mentioned
+Capitalized
+
+
+Answer = None of the mentioned
+#Which of the following are Python reserved words (keywords):
+default
+and
+goto
+class
+None
+
+Answer = class, and, none
