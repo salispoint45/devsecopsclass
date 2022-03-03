a3406be1948b77f43dcca8d908c8728a129ea08c
salispoint45 <obisco45@yahoo.com>
2022-03-02 23:14:09 -0500

    common python

---
+++ question_of_the_day_feb21.py
@@ -0,0 +1,34 @@
+Question of the day For Monday February 21st 2022
+#what is the output of the following code
+def calculate (num1, num2=4):
+  res = num1 * num2
+  print(res)
+calculate(5, 6)
+#What is the output of the following code?
+Answer:30
+
+var = "James" * 2  * 3
+print(var)
+#what is the output of the following code
+Answer:JamesJamesJamesJamesJamesJames
+
+x = 36 / 4 * (3 +  2) * 4 + 2
+print(x)
+#What is the output of the following code
+Answer: 182.00
+
+var1 = 1
+var2 = 2
+var3 = 3
+print(var1 + var2 + var3)
+Answer:6
+#what is the output of the following code?
+var1 = 1
+var2 = 2
+var3 = "3"
+print(var + var2 + var3)
+
+#what is the output of the following code?
+p, q, r = 10, 20 ,30
+print(p, q, r)
+answer:p, q, r = 10, 20 ,30
